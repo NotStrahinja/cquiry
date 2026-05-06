c0e9baacd80d4d4824d3535435a10c92186bc0d3
GottVonTerrorismus <124096168+NotStrahinja@users.noreply.github.com>
2026-05-06 15:04:36 +0200

    Update demo.c

--- demo.c
+++ demo.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    QC_Context ctx = {0};
+    QC_Context ctx = QC_DEFAULT_CONTEXT;
 
     char *name = QC_text(&ctx, "What is your name?", 16);
 
@@ -13,9 +13,9 @@ int main()
     char *select = QC_select(&ctx, "Select a fruit:", fruits, QC_ARRLEN(fruits));
 
     const char *features[] = {"Bla bla", "Demo", "123"};
-    uint64_t selected_features = QC_checkbox("Select features:", features, QC_ARRLEN(features));
+    uint64_t selected_features = QC_checkbox(&ctx, "Select features:", features, QC_ARRLEN(features));
 
-    bool confirm = QC_confirm("Finish demo?");
+    bool confirm = QC_confirm(&ctx, "Finish demo?");
 
     QC_free_all(&ctx);
 
