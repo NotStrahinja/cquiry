35ae5f0895ea121fe6d3682dbd766798c4945d4f
Strahinja <124096168+NotStrahinja@users.noreply.github.com>
2026-07-26 12:20:20 +0200

    Update demo.c

--- demo.c
+++ demo.c
@@ -8,12 +8,18 @@ int main()
     char *name = CQ_text(&ctx, "What is your name?", 16);
 
     char *password = CQ_password(&ctx, "Enter your password:", 16);
+    CQ_free_secure(&ctx, password); // Wipes the memory first then frees
 
-    const char *fruits[] = {"Apple", "Banana", "Orange"};
+    const char *fruits[] = { "Apple", "Banana", "Orange" };
     char *select = CQ_select(&ctx, "Select a fruit:", fruits, CQ_ARRLEN(fruits));
 
-    const char *features[] = {"Bla bla", "Demo", "123"};
+    const char *features[] = { "Bla bla", "Demo", "123" };
     uint64_t selected_features = CQ_checkbox(&ctx, "Select features:", features, CQ_ARRLEN(features));
+
+    if (CQ_CHECKED(selected_features, 0 /* Bla Bla */))
+    {
+        printf("[+] Bla Bla checked!\n");
+    }
 
     bool confirm = CQ_confirm(&ctx, "Finish demo?");
 
